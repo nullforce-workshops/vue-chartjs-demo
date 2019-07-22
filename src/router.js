be4199efd0cd4542aec71bc4bbd45160609a37be
nullforce <glenngit@nullforce.com>
2019-07-21 22:13:30 -0500

    Add line, bar, and bubble charts

--- src/router.js
+++ src/router.js
@@ -1,25 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Home from '@/views/Home'
+import VueChartJS from '@/views/VueChartJS'
+import VueChartKick from '@/views/VueChartKick'
+import VueCharts from '@/views/VueCharts'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'Home',
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/chartjs',
+      name: 'VueChartJS',
+      component: VueChartJS
+    },
+    {
+      path: '/chartkick',
+      name: 'VueChartKick',
+      component: VueChartKick
+    },
+    {
+      path: '/charts',
+      name: 'VueCharts',
+      component: VueCharts
     }
   ]
 })
---
+++ src/views/VueChartJS.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="container">
+    <h1>Demo examples of vue-chartjs</h1>
+    <div class="columns">
+      <div class="column">
+        <h3>Line Chart</h3>
+        <line-chart></line-chart>
+      </div>
+      <div class="column">
+        <h3>Bar Chart</h3>
+        <bar-chart></bar-chart>
+      </div>
+    </div>
+    <div class="columns">
+      <div class="column">
+        <h3>Bubble Chart</h3>
+        <bubble-chart></bubble-chart>
+      </div>
+      <div class="column">
+        <h3>Reactivity - Live update upon change in datasets</h3>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+  import LineChart from '@/components/LineChart'
+  import BarChart from '@/components/BarChart'
+  import BubbleChart from '@/components/BubbleChart'
+  import Reactive from '@/components/Reactive'
+
+  export default {
+    name: 'VueChartJS',
+    components: {
+      LineChart,
+      BarChart,
+      BubbleChart,
+      Reactive
+    }
+  }
+</script>
