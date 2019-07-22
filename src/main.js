4cd4cc929d6c98c4c0014fb533f4e4b837737ec6
nullforce <glenngit@nullforce.com>
2019-07-21 22:46:00 -0500

    Add chartkick charts

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import App from './App.vue'
 import router from './router'
 import 'chart.js'
 import 'hchs-vue-charts'
+import Chartkick from 'chartkick'
+import VueChartkick from 'vue-chartkick'
 
+Vue.use(VueChartkick, { Chartkick })
 Vue.use(window.VueCharts)
 Vue.config.productionTip = false
 
